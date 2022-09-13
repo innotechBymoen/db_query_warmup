05e07c65723b65b7242051413d7e9dce79717114
abymoen <a.bymoen@hotmail.com>
2022-09-13 17:02:57 -0600

    Addedd full backup and practice queries

---
+++ pokemon.sql
@@ -0,0 +1,58 @@
+-- MariaDB dump 10.19  Distrib 10.5.10-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: db_query_warmup
+-- ------------------------------------------------------
+-- Server version	10.5.10-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pokemon`
+--
+
+DROP TABLE IF EXISTS `pokemon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pokemon` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `type` varchar(15) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pokemon_un` (`name`),
+  CONSTRAINT `pokemon_check` CHECK (`type` = 'Fire' or `type` = 'Water' or `type` = 'Grass' or `type` = 'Electric' or `type` = 'test')
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pokemon`
+--
+
+LOCK TABLES `pokemon` WRITE;
+/*!40000 ALTER TABLE `pokemon` DISABLE KEYS */;
+INSERT INTO `pokemon` VALUES (1,'Charmander','Fire'),(2,'Pikachu','Electric'),(3,'Bulbasaur','Grass'),(4,'Magikarp','Water'),(5,'Squirtle','Water'),(12,'Charmeleon','Fire');
+/*!40000 ALTER TABLE `pokemon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_query_warmup'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-13 17:02:21
---
+++ queries.sql
@@ -0,0 +1,7 @@
+insert into pokemon(name, `type`) values('Charmander', 'Fire'), ('Pikachu', 'Electric'), ('Bulbasaur', 'Grass'), ('Magikarp', 'Water'), ('Squirtle', 'Water');
+
+select id, name, type from pokemon where type='Fire';
+
+delete from pokemon where name='test' or name='tester';
+
+update pokemon set `type`='Fire' where `type`='test';
